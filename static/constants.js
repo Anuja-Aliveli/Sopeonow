a4cf765bd2f2a6d9e3574f2455cd3496c30411a9
Anuja Aliveli <a.anuja54@gmail.com>
2024-10-09 09:20:27 +0530

    Icons styles modified, created constants.js, Applied bootstrap layout

---
+++ static/constants.js
@@ -0,0 +1,15 @@
+export const icons = [
+  'fa-magnifying-glass',
+  'fa-filter',
+  'fa-calculator',
+  'fa-truck-medical',
+  'fa-tv',
+  'fa-stethoscope',
+  'fa-tv',
+  'fa-house-user',
+  'fa-lock',
+  'fa-globe',
+  'fa-network-wired',
+  'fa-circle-nodes',
+  'fa-chart-column',
+];
--- static/script.js
+++ static/script.js
@@ -1,25 +1,10 @@
-const icons = [
-  'fa-magnifying-glass',
-  'fa-filter',
-  'fa-calculator',
-  'fa-truck-medical',
-  'fa-tv',
-  'fa-stethoscope',
-  'fa-tv',
-  'fa-house-user',
-  'fa-lock',
-  'fa-globe',
-  'fa-network-wired',
-  'fa-circle-nodes',
-  'fa-chart-column',
-];
+import { icons } from './constants.js';
 
 const sidebar = document.getElementById('sidebar');
 
-// Dynamically create and append icon spans
 icons.forEach((icon) => {
   const span = document.createElement('span');
-  span.classList.add('text-white', 'mt-2', 'sidebar-icon');
+  span.classList.add('text-white', 'sidebar-icon');
   span.innerHTML = `<i class="fa-solid ${icon}"></i>`;
   sidebar.appendChild(span);
 });
